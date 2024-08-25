f8a75ef4b8760bd998509ae4677b751bcbb1aff4
SebinSong <songxevin@gmail.com>
2024-08-25 17:58:58 +1200

    initial setup for GET /planets request

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+// routers
+const planetsRouter = require('./routes/planets/planets.router')
+
+// global middlewares
 app.use(express.json())
+
+// setup routes
+app.use('/planets', planetsRouter)
 
 module.exports = app
